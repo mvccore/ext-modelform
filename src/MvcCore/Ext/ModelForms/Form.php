cde71af020e721df070061063895a13e544cfaca
Tom Flídr <tomflidr@gmail.com>
2021-07-22 17:24:31 +0200

    release v5.1.14

--- src/MvcCore/Ext/ModelForms/Form.php
+++ src/MvcCore/Ext/ModelForms/Form.php
@@ -29,7 +29,7 @@ implements	\MvcCore\Ext\ModelForms\IForm {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.1.13';
+	const VERSION = '5.1.14';
 
 	/**
 	 * @inheritDocs
